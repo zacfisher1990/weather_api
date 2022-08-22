77c99b40689aad3db9209413a61a40c0d2852270
unknown <zcfshr@gmail.com>
2022-08-21 20:27:57 -0600

    fixed date/time, added weather icon

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ var date = document.getElementById("date");
 var temperature = document.getElementById("temperature");
 var humidity = document.getElementById("humidity");
 var wind = document.getElementById("wind");
+var icon = document.getElementById("icon");
+
+
+
 
 searchCityBtn.addEventListener('click', function(event){
     event.preventDefault()
 var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + document.getElementById("city").value.split(' ').join('+') + "&appid=1caf07bdfd403b26e4f9cc78d0d4e92b"
 console.log(weatherURL);
 console.log("show weather report");
-
-//city.textContent = document.getElementById("city").value;
-
 
 fetch(weatherURL)
     .then(function (response) {
@@ -21,7 +22,16 @@ fetch(weatherURL)
     .then(function (data) {
       console.log(data);
         city.textContent = data.name;
-        //date.textContent = today;
+        var todayDate = new Date();
+        var localTime = todayDate.getTime();
+        var localOffset = todayDate.getTimezoneOffset() * 60000;
+        var utc = localOffset + localTime;
+        var cityTime = utc + (1000 * data.timezone);
+        var displayTime = new Date(cityTime);
+        document.getElementById("time").textContent = displayTime;
+        var iconCode = data.weather[0].icon;
+        icon.src = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+        console.log(icon.src);
         temperature.textContent = "Temperature: " + Math.trunc(1.8 * (data.main.temp - 273) + 32) + "° F";
         humidity.textContent = "Humidity: " + data.main.humidity + "%";
         wind.textContent = "Wind Speed: " + data.wind.speed;
